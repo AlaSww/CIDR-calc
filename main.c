165ec5e581a7f546d6dda08ccd1df2ef1f4548b0
ala <hell86486@gmail.com>
2025-03-28 20:04:28 -0400

    final

--- main.c
+++ main.c
@@ -26,17 +26,51 @@ char* get_subnetmask(int cidr) {
 
     return result;  
 }
+int get_networks(){
+    int result=0;
+    while(result<=0){
+        printf("Enter the number of newtworks:");
+        scanf("%d",&result);
+    }
+    return(result);
+}
 
+void sortIndexes(int arr[], int indices[], int n) {
+    for (int i = 0; i < n; i++) {
+        indices[i] = i;
+    }
+
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[indices[j]] < arr[indices[j + 1]]) { 
+                int temp = indices[j];
+                indices[j] = indices[j + 1];
+                indices[j + 1] = temp;
+            }
+        }
+    }
+}
 int main() {
-    int hosts = 100;
-    int cidr = get_cidr_notation(hosts);  
-    printf("CIDR Notation: /%d\n", cidr);
-    
-
-    char *subnetmask = get_subnetmask(cidr);
-    printf("Subnet Mask: %s\n", subnetmask);
-
-    free(subnetmask);
-
+    int networks=get_networks();
+    int hostslist[networks];
+    int indices[networks];
+    for(int i=1;i<=networks;i++){
+        int hosts=0;
+        printf("Number of hosts of network %d: \n",i);
+        scanf("%d",&hosts);
+        hostslist[i-1]=hosts;
+    }
+    sortIndexes(hostslist,indices,networks);
+    for(int i=0;i<networks;i++){
+        printf("Network %d:\n",indices[i]+1);
+        int cidr = get_cidr_notation(hostslist[indices[i]]);
+        int usedhosts= pow(2,32-cidr);
+        printf("CIDR Notation: /%d\n", cidr);
+        char *subnetmask = get_subnetmask(cidr);
+        printf("Subnet Mask: %s\n", subnetmask);
+        free(subnetmask);
+        printf("add this value to the previous address:%d \n\n\n",usedhosts+1);
+    }
     return 0;
 }
